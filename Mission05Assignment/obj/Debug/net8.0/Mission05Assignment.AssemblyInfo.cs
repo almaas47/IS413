88d70a51d81846ba74951e2c9526af711b9c65a5
almaas47 <matt47@byu.edu>
2025-02-07 23:23:59 -0700

    Commit changes

--- Mission05Assignment/obj/Debug/net8.0/Mission05Assignment.AssemblyInfo.cs
+++ Mission05Assignment/obj/Debug/net8.0/Mission05Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission05Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d86cd02c701281fe49c8cfb97f2df20b50b9454")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission05Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission05Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
